0cf0c9df87290ebb0a894121eb3b3c695bc32c81
aydam2728 <lesdmart@gmail.com>
2023-02-08 11:13:08 +0100

    add parameters

---
+++ src/Modeèle.java
@@ -0,0 +1,8 @@
+import java.awt.Color;
+
+public class Modeèle {
+	Color[] COULEURS = {Color.YELLOW,Color.GREEN,Color.BLUE,Color.MAGENTA,Color.RED,Color.ORANGE,Color.BLACK,Color.WHITE};
+	int N_TENTATIVES;
+	int DIFFICULTE;
+	enum Etat {ENEN_COURS,GAGNé,PERDU};
+}
