cfa9098c9a8fba719153cb9e90597d3c75b6ec16
aydam2728 <lesdmart@gmail.com>
2023-02-08 11:17:23 +0100

    add some ...

--- src/Modeèle.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Color;
-
-public class Modeèle {
-	Color[] COULEURS = {Color.YELLOW,Color.GREEN,Color.BLUE,Color.MAGENTA,Color.RED,Color.ORANGE,Color.BLACK,Color.WHITE};
-	int N_TENTATIVES;
-	int DIFFICULTE;
-	enum Etat {ENEN_COURS,GAGNé,PERDU};
-}
---
+++ src/Modèle.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class Modèle {
+	public static final Color[] COULEURS = {Color.YELLOW,Color.GREEN,Color.BLUE,Color.MAGENTA,Color.RED,Color.ORANGE,Color.BLACK,Color.WHITE};
+	public static final int N_TENTATIVES = 0;
+	public static final int DIFFICULTE =0 ;
+	public static enum Etat {ENEN_COURS,GAGNé,PERDU};
+	
+	public Modèle(){
+		
+	}
+}
