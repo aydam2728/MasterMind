5d8f537591db90b45e31c977f85f1b1744a47b56
Gogack <franckgarciagonzalez@gmail.com>
2023-02-08 01:53:36 +0100

    Changement sur rangée
    modification taille

--- src/Rangée.java
+++ src/Rangée.java
@@ -1,17 +1,17 @@
 import java.awt.Color;
 
 public class Rangée {
-	Color[] jetons;
-	int indiceJetons;
+	Color[] jetons = new Color[4];
+	int indiceJetons = 0;
 	public Rangée(Color[] jet) {
 		jet = this.jetons;
 	}
 	public void ligne(Color[] jet) {
 		jet = this.jetons;
-		if (this.indiceJetons == Modèle.DIFFICULTE ) {
+		if (this.indiceJetons == 4) {
 			System.out.println("La ligne est complète");
 		}
-		else if (0 < this.indiceJetons && this.indiceJetons < Modèle.DIFFICULTE) {
+		else if (0 < this.indiceJetons && this.indiceJetons < 4) {
 			System.out.println("La ligne est entamée");
 		}else {
 			System.out.println("La ligne n'est pas commencé");
