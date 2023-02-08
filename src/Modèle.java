8413fa213f4f69f3895700a5f2f6edd968943479
aydam2728 <lesdmart@gmail.com>
2023-02-08 11:32:17 +0100

    add Constructor

--- src/Modèle.java
+++ src/Modèle.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Random;
 
 public class Modèle {
 	public static final Color[] COULEURS = {Color.YELLOW,Color.GREEN,Color.BLUE,Color.MAGENTA,Color.RED,Color.ORANGE,Color.BLACK,Color.WHITE};
@@ -6,7 +7,19 @@ public class Modèle {
 	public static final int DIFFICULTE =0 ;
 	public static enum Etat {ENEN_COURS,GAGNé,PERDU};
 	
+	Etat état;
+	Rangée combinaison;
+	Rangée[] prpopositions;
+	int tentative;
 	public Modèle(){
-		
+		Random rand = new Random();
+		Color[] temp= new Color[4];
+		for (int i = 0; i < 4; i++) {
+	        int randomIndex = rand.nextInt(this.COULEURS.length);
+	        temp[i]= this.COULEURS[randomIndex];
+	    }
+		this.combinaison = new Rangée(temp);
 	}
+	
+	
 }
