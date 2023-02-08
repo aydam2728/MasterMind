9b7b4f48f748f79c7a55995ea56e286d0b96645a
Gogack <franckgarciagonzalez@gmail.com>
2023-02-08 01:44:45 +0100

    Update Rangée.java
    coucou

--- src/Rangée.java
+++ src/Rangée.java
@@ -1,4 +1,20 @@
+import java.awt.Color;
 
 public class Rangée {
-
+	Color[] jetons;
+	int indiceJetons;
+	public Rangée(Color[] jet) {
+		jet = this.jetons;
+	}
+	public void ligne(Color[] jet) {
+		jet = this.jetons;
+		if (this.indiceJetons == Modeèe.Difficulté ) {
+			System.out.println("La ligne est complète");
+		}
+		else if (0 < this.indiceJetons && this.indiceJetons < Modeèe.Difficulté) {
+			System.out.println("La ligne est entamée");
+		}else {
+			System.out.println("La ligne n'est pas commencé");
+		}
+	}
 }
